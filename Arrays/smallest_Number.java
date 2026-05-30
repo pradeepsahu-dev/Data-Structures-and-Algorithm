219511581448fc522667b72cabd670cb63aa480c
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-30 13:43:11 +0530

    find smallest number

---
+++ Arrays/smallest_Number.java
@@ -0,0 +1,18 @@
+public class smallest_Number {
+
+    public static int smallest_nums(int arr[]){
+        int smallest = Integer.MAX_VALUE;         // show + Infinity
+        for(int i=0; i<arr.length; i++){
+            if(smallest > arr[i]){
+                smallest = arr[i];               // smallest number
+
+            }
+        }
+        return smallest;
+    }
+    public static void main(String[]args){
+        int arr[] = {3,6,8,1,4,0};
+        System.out.println("Smallest number is :" + smallest_nums(arr));
+    }
+    
+}
