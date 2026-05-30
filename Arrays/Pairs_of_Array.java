6523a93367ac4cc52f28892095fc4383b27b1e94
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-05-30 23:30:12 +0530

    pairs of array with total pair

--- Arrays/Pairs_of_Array.java
+++ Arrays/Pairs_of_Array.java
@@ -1,13 +1,17 @@
 public class Pairs_of_Array {
 
     public static void pair_in_Array(int arr[]){
+        int totalaPair = 0;
         for(int i=0; i<=arr.length-1; i++){
             int curr = arr[i];
             for(int j=i+1; j<=arr.length-1; j++){
                 System.out.print("("+curr+","+arr[j]+")"+" ");
+                totalaPair++;
             }
             System.out.println();
         }
+          System.out.println("Total pairs is:"+totalaPair);
+
     }
     public static void main(String[]args){
         int arr[] = {2,4,6,8,12};
