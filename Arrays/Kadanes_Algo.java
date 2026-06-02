4d336b289e053a47b0dc71fcdbe818b0e413ed02
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-06-03 03:36:01 +0530

    max subArray sum using kadane's algorithm

---
+++ Arrays/Kadanes_Algo.java
@@ -0,0 +1,26 @@
+public class Kadanes_Algo {
+
+    public static void kadane(int arr[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<=arr.length-1; i++){
+
+            currSum += arr[i];
+
+            if(currSum < 0){
+                currSum = 0;
+            }
+
+            maxSum = Math.max(currSum , maxSum);
+
+        }
+        System.out.println(maxSum);
+
+    }
+
+    public static void main(String[]args){
+        int arr[] = {-2,-3,4,-1,-2,1,5,-3};
+        kadane(arr);
+    }
+    
+}
