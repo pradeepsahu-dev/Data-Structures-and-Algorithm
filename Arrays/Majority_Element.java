dc0127cae99a7d9d375917a7bd0c8c5f0ac914b7
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-06-10 16:41:21 +0530

    Majority Element optimize using HashMap

--- Arrays/Majority_Element.java
+++ Arrays/Majority_Element.java
@@ -1,9 +1,36 @@
+import java.util.HashMap;
+
 public class Majority_Element {
 
+    //optimized 
+    public static int MajorityElement(int arr[]){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : arr){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+
+            }else{
+                map.put(num,1);
+            }
+        }
+        // majority check
+
+        for(int num : arr){
+            if(map.get(num) > arr.length/2){
+                return num;
+            }
+        }
+        return -1;
+   
+    }
+   
+
+
+
+    // Brute force appoach
     public static int majorityElement(int arr[]){
         for(int i=0; i<arr.length; i++){
             int count = 1;
-            int val = i;
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i] == arr[j]){
                     count ++;
@@ -11,7 +38,7 @@ public class Majority_Element {
             } 
 
             if(count > arr.length/2){
-                return arr[val];
+                return arr[i];
 
             }
             
@@ -19,8 +46,8 @@ public class Majority_Element {
         return -1;
     }
     public static void main(String[]args){
-        int arr[] = {2,1,1,1,1,2,5};
-        System.out.println(majorityElement(arr));
+        int arr[] = {2,2,1,1,1,2,2};
+        System.out.println(MajorityElement(arr));
     }
     
 }
