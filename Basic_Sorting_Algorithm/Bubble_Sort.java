fb26cc4ca575391d0f53b1ff493c9f169dfd7a3f
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-06-11 03:25:08 +0530

    Bubble sort Optimized

--- Basic_Sorting_Algorithm/Bubble_Sort.java
+++ Basic_Sorting_Algorithm/Bubble_Sort.java
@@ -2,12 +2,18 @@ public class Bubble_Sort {
 
     public static void bubbleSort(int arr[]){
         for(int i=0; i<arr.length-1; i++){
+            boolean swapped = false;  // to check if any swap happend in this pass
+            
             for(int j=0; j<arr.length-1-i; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;    // means swapping happend
                 }
+            }
+            if(swapped == false){
+                break;
             }
         }
         
