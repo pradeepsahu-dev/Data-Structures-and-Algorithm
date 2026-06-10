ca4021d3c5737416b403b0154e36d7a6b348bf80
Pradeep Sahu <pradeepsahu29885@gmail.com>
2026-06-11 04:21:00 +0530

    Selection Sort completed.

--- Basic_Sorting_Algorithm/Bubble_Sort.java
+++ Basic_Sorting_Algorithm/Bubble_Sort.java
@@ -3,7 +3,7 @@ public class Bubble_Sort {
     public static void bubbleSort(int arr[]){
         for(int i=0; i<arr.length-1; i++){
             boolean swapped = false;  // to check if any swap happend in this pass
-            
+
             for(int j=0; j<arr.length-1-i; j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
